74eeebc949b7d685fca4fac8330a6cd33b6d2ddc
zhangchao <1592727414@qq.com>
2020-12-18 20:27:10 +0800

    功能基本实现

--- src/Login.java
+++ src/Login.java
@@ -26,21 +26,23 @@ public class Login {
 
 //    public final static List<Map.Entry<Student,Integer>> rankList= new ArrayList<>();
     public static void main(String[] args) {
+        boolean isInputGrade=false;
         Student student1=new Student("zhang3","123456","张3","333333",false,0,120);
         Student student2=new Student("zhang4","123456","张4","444444",true,2,120);
         Student student3=new Student("zhang5","123456","张5","555555",true,1,120);
         Student student4=new Student("zhang6","123456","张6","666666",true,1,120);
-        Student student5=new Student("zhang7","123456","张7","777777",false,0,120);
+        Student student5=new Student("zhang7","123456","张7","777777",true,2,80);
 
         Administrator adm1=new Administrator("管理员01","456789");
         Administrator adm2=new Administrator("管理员02","456789");
         Administrator adm3=new Administrator("管理员03","456789");
 
+        int count=2;
         Message message1=new Message(1,"asdfasdfasdfasdfasdfasdfasdfasdf");
         Message message2=new Message(2,"qwerqwerqwerqwerqwerqwerqwer");
-        Map<Integer,Message> totalMessage=new HashMap<>();
-        totalMessage.put(message1.getCount(),message1);
-        totalMessage.put(message2.getCount(),message2);
+        Map<Integer,String> totalMessage=new HashMap<>();
+        totalMessage.put(message1.getCount(),message1.getMessage());
+        totalMessage.put(message2.getCount(),message2.getMessage());
 
         Map<String,Student> totalStudent=new HashMap<>();
         totalStudent.put(student1.getUserName(),student1);
@@ -53,9 +55,7 @@ public class Login {
         totalAdm.put(adm1.getUserName(),adm1);
         totalAdm.put(adm2.getUserName(),adm2);
         totalAdm.put(adm3.getUserName(),adm3);
-//        Iterator iterA=totalAdm.entrySet().iterator();
 
-        int count=2;
         boolean isFinished=false;
 
         Scanner input=new Scanner(System.in);
@@ -65,6 +65,7 @@ public class Login {
             System.out.println("=====================================");
             System.out.print("是否登录为管理员(1为是，0为否):");
             int isAdm=input.nextInt();
+            //学生端登录
             if(isAdm==0){
                 System.out.println("=====================================");
                 System.out.print("是否注册(1为是，0为否):");
@@ -84,7 +85,6 @@ public class Login {
                     totalStudent.put(studentLogon.getUserName(),studentLogon);
                     System.out.println("注册成功！！,请重新登录");
                 }//注册功能
-//                while(true){
                     boolean isIN=false;
                     System.out.println("=====================================");
                     System.out.print("用户名：");
@@ -102,9 +102,9 @@ public class Login {
                     }
                     //登录成功的情况
                     if(isIN==true){
+                        System.out.println("=====================================");
                         System.out.println("登录成功！！");
                         Student studentData=totalStudent.get(username);//浅拷贝
-//                        System.out.println(totalStudent.get(username).getIsJoin());
                         while(true){
                             int action;
                             System.out.println("=====================================");
@@ -121,12 +121,23 @@ public class Login {
                                     System.out.println("=====================================");
                                     System.out.println("报名活动已经结束。");
                                 }
-                                else{
+                                else if(studentData.getIsJoin()==false&&studentData.getIsPassed()==0){
                                     studentData.setIsJoin(true);
                                     studentData.setIsPassed(1);
-//                                System.out.println(totalStudent.get(username).getIsJoin());
                                     System.out.println("=====================================");
                                     System.out.println("报名成功，等待审核！！");
+                                }
+                                else if(studentData.getIsPassed()==1){
+                                    System.out.println("=====================================");
+                                    System.out.println("报名申请中，不能重复报名");
+                                }
+                                else if(studentData.getIsPassed()==2){
+                                    System.out.println("=====================================");
+                                    System.out.println("报名已通过，不可重复报名");
+                                }
+                                else if(studentData.getIsPassed()==3){
+                                    System.out.println("=====================================");
+                                    System.out.println("报名申请被拒绝，不可再报名");
                                 }
                             }//参加报名
                             if(action==1){
@@ -156,18 +167,49 @@ public class Login {
                                 }
                                 else{
                                     System.out.println("=====================================");
-                                    System.out.println("这里还没有写");
+                                    if(studentData.getIsPassed()==1){
+                                        System.out.println("审核中,没有成绩");
+                                    }
+                                    else if(studentData.getIsPassed()==3){
+                                        System.out.println("审核中,没有成绩");
+                                    }
+                                    else if(studentData.getIsPassed()==0){
+                                        System.out.println("未报名");
+                                    }
+                                    else{
+                                        if(studentData.getGrade()>100){
+                                            System.out.println("成绩未录入,请等待");
+                                        }
+                                        else{
+                                            int rank=0;
+                                            Iterator iterRank=sortDescend(studentHasGrade).entrySet().iterator();
+                                            while (iterRank.hasNext()){
+                                                rank++;
+                                                Map.Entry entryRank=(Map.Entry) iterRank.next();
+                                                Student studentDataR=((Student)entryRank.getKey());
+                                                if (studentDataR.getUserName().equals(username)) {
+                                                    System.out.println("姓名为:"+studentDataR.getTrueName() + "  成绩为:" + studentDataR.getGrade() + " 排名为:" + rank);
+                                                    break;
+                                                }
+                                            }
+                                        }
+                                    }
 
                                 }
                             }//查看成绩和排名
                             if(action==3){
                                 System.out.println("=====================================");
-                                System.out.println("查看大赛总成绩（为写）");
+                                System.out.println("以下为前10排名(人数未满10的话输出总榜)");
+                                int rank=0;
                                 Iterator iterRank=sortDescend(studentHasGrade).entrySet().iterator();
                                 while (iterRank.hasNext()){
+                                    rank++;
                                     Map.Entry entryRank=(Map.Entry) iterRank.next();
                                     Student studentDataR=((Student)entryRank.getKey());
-                                    System.out.println(studentDataR.getTrueName()+"  "+studentDataR.getGrade());
+                                    System.out.println("姓名:"+studentDataR.getTrueName()+"  成绩为:"+studentDataR.getGrade()+" 排名为"+rank);
+                                    if(rank==10){
+                                        break;
+                                    }
                                 }
                             }//大赛总排名
                             if(action==4){
@@ -175,17 +217,17 @@ public class Login {
                                 Iterator iterM=totalMessage.entrySet().iterator();
                                 while(iterM.hasNext()){
                                     Map.Entry entryM=(Map.Entry)iterM.next();
-                                    System.out.println("第"+((Message)entryM.getValue()).getCount()+"条信息："+
-                                            ((Message)entryM.getValue()).getMessage());
+                                    System.out.println("第"+entryM.getKey()+"条信息："+
+                                            entryM.getValue());
                                 }
                             }//查看信息
-                            if(action==5)
+                            if(action==5){
                                 break;
+                            }
                         }
                     }else {
                         System.out.println("登录失败,用户名或密码错误,请重新输入");
                     }
-//                }
             }
             //管理员登录
             else{
@@ -215,10 +257,12 @@ public class Login {
                         System.out.println("4为录入比赛成绩");
                         System.out.println("5为查看运动员成绩和排名");
                         System.out.println("6为查看总排名");
-                        System.out.println("7为退出账号");
+                        System.out.println("7为发布信息");
+                        System.out.println("8为退出账号");
                         System.out.print("请输入行为:");
                         int action=input.nextInt();
-                        if(action==7){
+                        if(action==8){
+                            System.out.println("=====================================");
                             System.out.println("已退出");
                             break;
                         }//退出登录
@@ -233,6 +277,7 @@ public class Login {
                             System.out.println("操作完成");
                         }//启停活动
                         if(action==2){
+                            System.out.println("=====================================");
                             int countNum=0;
                             Iterator iterS=totalStudent.entrySet().iterator();
                             while(iterS.hasNext()){
@@ -260,6 +305,7 @@ public class Login {
                             }
                         }//审批
                         if(action==3){
+                            System.out.println("=====================================");
                             int countPassed=0;
                             Iterator iterS=totalStudent.entrySet().iterator();
                             while (iterS.hasNext()){
@@ -274,6 +320,7 @@ public class Login {
                             System.out.println("参赛人数为:"+countPassed+"人");
                         }//查看和统计报名情况
                         if(action==4){
+                            System.out.println("=====================================");
                             Iterator iterS=totalStudent.entrySet().iterator();
                             while (iterS.hasNext()){
                                 Map.Entry entryS=(Map.Entry)iterS.next();
@@ -285,24 +332,81 @@ public class Login {
                                     studentHasGrade.put(studentData,studentData.getGrade());//录入有成绩的学生
                                 }
                             }
+                            isInputGrade=true;
                         }//录入比赛成绩
                         if(action==5){
                             System.out.println("=====================================");
-                            Iterator iterS=studentHasGrade.entrySet().iterator();
-                            while (iterS.hasNext()){
-                                Map.Entry entryR=(Map.Entry)iterS.next();
-                                Student studentData=((Student)entryR.getKey());
-                                System.out.println(studentData.getTrueName()+
-                                        " "+studentData.getUserId()+"");
+                            if(isInputGrade==false){
+                                System.out.println("成绩未录入，请先录入成绩！！");
                             }
-                            Iterator iterRank=sortDescend(studentHasGrade).entrySet().iterator();
-                            while (iterRank.hasNext()){
-                                Map.Entry entryRank=(Map.Entry) iterRank.next();
-                                Student studentData=((Student)entryRank.getKey());
-                                System.out.println(studentData.getTrueName()+"  "+studentData.getGrade());
+                            else{
+                                while(true){
+                                    System.out.println("0为退出");
+                                    System.out.println("1为查找");
+                                    if(input.nextInt()==0){
+                                        break;
+                                    }
+                                    if(input.nextInt()==1){
+                                        System.out.print("输入要查找的人的真实姓名");
+                                        String name=input.next();
+                                        boolean isFind=false;
+                                        int rank=0;
+                                        Iterator iterRank=sortDescend(studentHasGrade).entrySet().iterator();
+                                        while (iterRank.hasNext()){
+                                            rank++;
+                                            Map.Entry entryRank=(Map.Entry) iterRank.next();
+                                            Student studentData=((Student)entryRank.getKey());
+                                            if (studentData.getTrueName().equals(name)) {
+                                                System.out.println(studentData.getTrueName() + "  " + studentData.getGrade() + " 排名为" + rank);
+                                                isFind = true;
+                                                break;
+                                            }
+                                        }
+                                        if(isFind=false){
+                                            System.out.println("查无此人或此人为参加比赛！！");
+                                        }
+                                    }
+                                }
                             }
-
                         }//查看运动员成绩和排名
+                        if(action==6){
+                            System.out.println("=====================================");
+                            if(isInputGrade==false){
+                                System.out.println("成绩为录入，请先录入成绩！！");
+                            }
+                            else{
+                                int rank=0;
+                                Iterator iterRank=sortDescend(studentHasGrade).entrySet().iterator();
+                                while (iterRank.hasNext()){
+                                    rank++;
+                                    Map.Entry entryRank=(Map.Entry) iterRank.next();
+                                    Student studentData=((Student)entryRank.getKey());
+                                    System.out.println(studentData.getTrueName()+"  "+studentData.getGrade()+" 排名为"+rank);
+                                }
+                            }
+                        }//查看总排名
+                        if(action==7){
+                            while(true){
+                                System.out.println("=====================================");
+                                System.out.println("是否发布信息(1为是，0为否):");
+                                int isMessge=input.nextInt();
+                                if(isMessge==1){
+                                    count++;
+                                    System.out.println("=====================================");
+                                    System.out.print("输入要发布的信息:");
+                                    String string=input.next();
+                                    Message message=new Message(count,string);
+                                    totalMessage.put(message.getCount(),message.getMessage());
+                                    System.out.println("=====================================");
+                                    System.out.println("信息发布成功");
+                                }
+                                else{
+                                    System.out.println("=====================================");
+                                    System.out.println("已退出发布信息功能！！");
+                                    break;
+                                }
+                            }
+                        }//发布信息
                     }
                 }
                 else{
